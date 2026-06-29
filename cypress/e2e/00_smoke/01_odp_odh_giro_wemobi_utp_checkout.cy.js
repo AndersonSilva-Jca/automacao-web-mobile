efcb977c629a440c529ecc116973be58d5192945
Anderson Silva dos Santos <anderson.ssantos@jcatlm.com.br>
2026-06-29 11:58:36 -0300

    refatoração clube giro deploy

--- cypress/e2e/00_smoke/01_odp_odh_giro_wemobi_utp_checkout.cy.js
+++ cypress/e2e/00_smoke/01_odp_odh_giro_wemobi_utp_checkout.cy.js
@@ -66,45 +66,45 @@ describe("ODH, ODP, Giro, Wemobi, UTP ", () => {
     });
   });
 
-  it("Giro - Deve fazer login, busca de destinos, selecionar datas, seleção de passagens, selecionar assentos", () => {
-    cy.env(["login2", "senha"]).then((env) => {
-      cy.visit(giro);
-      cy.get(loc.GIRO_BOTAO_LOGIN).should("be.visible").click();
-      cy.get(".login-title > p").should("contain", "Acesse o Giro");
-      cy.get(loc.USUARIO).should("be.visible").type("andynho1987@gmail.com", { delay: 100 });
-      cy.get(loc.SENHA).should("be.visible").type(env.senha, { log: false }, { delay: 100 });
-      cy.get(loc.GIRO_BOTAO_ENTRAR).click({ force: true });
-      cy.wait(4000);
-    });
-    cy.get("body").then(($body) => {
-      const temModal2FA = $body.find('input[data-js="modal-input-password-twofa"]:visible').length > 0;
-      if (temModal2FA) {
-        cy.log("🔐 Modal 2FA detectado e visível – buscando código no e-mail...");
-        cy.wait(4000);
-        cy.task("buscarCodigo2FAGmail").then((codigo2FA) => {
-          expect(codigo2FA).to.not.be.null;
-          cy.get('input[data-js="modal-input-password-twofa"]').focus().clear({ force: true }).type(codigo2FA, { force: true, delay: 80 }); // force ignora visibility
-          cy.get('button[data-js="modal-button-twofa"]').should("not.be.disabled").click();
-        });
-      } else {
-        cy.log("✅ Login direto – Modal 2FA está oculto (display: none). Pulando etapa.");
-      }
-    });
-    cy.get(loc.MENSAGEM_LOGADO).should("contain", "ANDERSON");
-    cy.get(loc.BUSCAS.DESTINO_IDA).click().type("Rio De Janeiro - Todos (RJ)", { delay: 100 });
-    cy.contains(" Rio De Janeiro - Todos (RJ) ").click({ force: true });
-    cy.get(loc.BUSCAS.DESTINO_VOLTA).click().type("São Paulo - Todos (SP)", { delay: 100 });
-    cy.contains("São Paulo - Todos (SP)").click({ force: true });
-    cy.get(loc.BUSCAS.DATA_IDA).click();
-    cy.get(loc.LOADER).should("not.exist");
-    cy.selecionarDataIda(5);
-    cy.get(loc.BUSCAS.BOTAO_BUSCAR, { timeout: 90000 }).should("be.visible").click();
-    cy.selecionarPassagemAleatoria1({ timeout: 90000 });
-    cy.get(loc.CHECK_PASSAGEIRO, { timeout: 90000 }).click({ force: true });
-    cy.get(loc.BOTAO_AVANCAR).should("be.visible").and("not.be.disabled").click();
-    cy.selecionarAssentoAleatorio({ timeout: 90000 });
-    cy.get(loc.BOTAO_AVANCAR).should("be.visible").click();
-  });
+  // it("Giro - Deve fazer login, busca de destinos, selecionar datas, seleção de passagens, selecionar assentos", () => {
+  //   cy.env(["login2", "senha"]).then((env) => {
+  //     cy.visit(giro);
+  //     cy.get(loc.GIRO_BOTAO_LOGIN).should("be.visible").click();
+  //     cy.get(".login-title > p").should("contain", "Acesse o Giro");
+  //     cy.get(loc.USUARIO).should("be.visible").type("andynho1987@gmail.com", { delay: 100 });
+  //     cy.get(loc.SENHA).should("be.visible").type(env.senha, { log: false }, { delay: 100 });
+  //     cy.get(loc.GIRO_BOTAO_ENTRAR).click({ force: true });
+  //     cy.wait(4000);
+  //   });
+  //   cy.get("body").then(($body) => {
+  //     const temModal2FA = $body.find('input[data-js="modal-input-password-twofa"]:visible').length > 0;
+  //     if (temModal2FA) {
+  //       cy.log("🔐 Modal 2FA detectado e visível – buscando código no e-mail...");
+  //       cy.wait(4000);
+  //       cy.task("buscarCodigo2FAGmail").then((codigo2FA) => {
+  //         expect(codigo2FA).to.not.be.null;
+  //         cy.get('input[data-js="modal-input-password-twofa"]').focus().clear({ force: true }).type(codigo2FA, { force: true, delay: 80 }); // force ignora visibility
+  //         cy.get('button[data-js="modal-button-twofa"]').should("not.be.disabled").click();
+  //       });
+  //     } else {
+  //       cy.log("✅ Login direto – Modal 2FA está oculto (display: none). Pulando etapa.");
+  //     }
+  //   });
+  //   cy.get(loc.MENSAGEM_LOGADO).should("contain", "ANDERSON");
+  //   cy.get(loc.BUSCAS.DESTINO_IDA).click().type("Rio De Janeiro - Todos (RJ)", { delay: 100 });
+  //   cy.contains(" Rio De Janeiro - Todos (RJ) ").click({ force: true });
+  //   cy.get(loc.BUSCAS.DESTINO_VOLTA).click().type("São Paulo - Todos (SP)", { delay: 100 });
+  //   cy.contains("São Paulo - Todos (SP)").click({ force: true });
+  //   cy.get(loc.BUSCAS.DATA_IDA).click();
+  //   cy.get(loc.LOADER).should("not.exist");
+  //   cy.selecionarDataIda(5);
+  //   cy.get(loc.BUSCAS.BOTAO_BUSCAR, { timeout: 90000 }).should("be.visible").click();
+  //   cy.selecionarPassagemAleatoria1({ timeout: 90000 });
+  //   cy.get(loc.CHECK_PASSAGEIRO, { timeout: 90000 }).click({ force: true });
+  //   cy.get(loc.BOTAO_AVANCAR).should("be.visible").and("not.be.disabled").click();
+  //   cy.selecionarAssentoAleatorio({ timeout: 90000 });
+  //   cy.get(loc.BOTAO_AVANCAR).should("be.visible").click();
+  // });
 
   it("Wemobi - Deve fazer login, busca de destinos, selecionar datas, seleção de passagens, selecionar assentos", () => {
     cy.env(["login", "senha"]).then((env) => {
