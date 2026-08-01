684b24b4a69d79c241b6210def02fdcdb221f800
Anderson Silva dos Santos <anderson.ssantos@jcatlm.com.br>
2026-08-01 15:36:01 -0300

    refatorando o commands

--- cypress/pages/SearchPage.js
+++ cypress/pages/SearchPage.js
@@ -6,13 +6,16 @@ class SearchPage {
     cy.get(loc.BUSCAS.DESTINO_IDA).click().type(loc.SP_TIETE, { delay: 100 }).should("exist").invoke("show");
     cy.xpath(loc.XPATH_SP_TIETE).click({ force: true });
   }
+
   buscaDestino() {
     cy.get(loc.BUSCAS.DESTINO_VOLTA).click().type(loc.RJ_TODOS, { delay: 100 }).should("exist").invoke("show");
     cy.xpath(loc.XPATH_RJ_TODOS).click({ force: true });
   }
+
   confirmarBusca() {
     cy.get(loc.BUSCAS.BOTAO_BUSCAR, { timeout: 90000 }).should("be.visible").click();
   }
+
   dataIda() {
     cy.get(loc.BUSCAS.DATA_IDA).click();
     cy.selecionarDataIda(4);
@@ -27,13 +30,16 @@ class SearchPage {
     cy.get(`[cmdk-item][data-value="${cidadeSorteada}"]`).first().click({ force: true });
     cy.log(`🏙️ Destino sorteado e selecionado para o teste: ${cidadeSorteada}`);
   }
+
   odhCheckIn() {
     cy.get(loc.ODH_CHECKIN).click();
     cy.selecionarPeriodoEstadia(3);
   }
+
   odhConfirmarBusca() {
     cy.get(loc.ODH_BOTAO_PROCURAR).click();
   }
+
   odhLoaderBusca() {
     cy.get(loc.ODH_LOADER_BUSCA).should("be.visible");
   }
@@ -44,14 +50,17 @@ class SearchPage {
     cy.get(loc.BUSCAS.DESTINO_IDA).click().type("São Paulo - Rodoviária Tietê (SP)", { delay: 100 });
     cy.xpath(loc.ODP_XPATH_SP_TIETE).click({ force: true });
   }
+
   odpBuscaDestino() {
     cy.get(loc.BUSCAS.DESTINO_VOLTA).click().type("Rio De Janeiro - Todos (RJ)", { delay: 100 });
     cy.xpath(loc.ODP_XPATH_RJ_TODOS).click({ force: true });
   }
+
   odpDataIda() {
     cy.get(loc.BUSCAS.DATA_IDA).click();
     cy.selecionarDataIda(5);
   }
+
   odpConfirmarBusca() {
     cy.get(loc.BUSCAS.BOTAO_BUSCAR, { timeout: 90000 }).should("be.visible").click();
   }
@@ -64,16 +73,19 @@ class SearchPage {
     cy.get(loc.BUSCAS.DESTINO_IDA).click().type("São Paulo - Todos (SP)", { delay: 100 });
     cy.contains("São Paulo - Todos (SP)").click({ force: true });
   }
+
   giroBuscaDestino() {
     cy.get(loc.BUSCAS.DESTINO_VOLTA).click().type("Rio De Janeiro - Todos (RJ)", { delay: 100 });
     cy.contains(" Rio De Janeiro - Todos (RJ) ").click({ force: true });
   }
+
   giroDataIda() {
     cy.get(loc.BUSCAS.DATA_IDA).click();
     // cy.get(loc.LOADER).should("not.exist");
     cy.selecionarDataIda(5);
     cy.fecharModalGiro();
   }
+
   giroConfirmarBusca() {
     cy.fecharModalGiro();
     cy.get(loc.BUSCAS.BOTAO_BUSCAR, { timeout: 90000 }).should("be.visible").click();
@@ -84,14 +96,17 @@ class SearchPage {
     cy.get(loc.BUSCAS.DESTINO_IDA).click().type("São Paulo - Rodoviária Tietê (SP)", { delay: 100 });
     cy.xpath(loc.WEMOBI_XPATH_SP).click({ force: true });
   }
+
   wemobiBuscaDestino() {
     cy.get(loc.BUSCAS.DESTINO_VOLTA).click().type("Rio De Janeiro - Todos (RJ)", { delay: 100 });
     cy.xpath(loc.WEMOBI_XPATH_RJ).click({ force: true });
   }
+
   wemobiDataIda() {
     cy.get(loc.BUSCAS.DATA_IDA).click();
     cy.selecionarDataIda(5);
   }
+
   wemobiConfirmarBusca() {
     cy.get(loc.BUSCAS.BOTAO_BUSCAR, { timeout: 90000 }).should("be.visible").click();
   }
