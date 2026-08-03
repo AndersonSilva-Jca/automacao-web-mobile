8fdb7c4f2deeba1eea1f697ee9e8f72b14ca71e6
Anderson Silva dos Santos <anderson.ssantos@jcatlm.com.br>
2026-08-03 16:15:30 -0300

    ajustes nos testes de regressão

--- cypress/e2e/Regressão/Catarinense/catarinense_regressão.cy.js
+++ cypress/e2e/Regressão/Catarinense/catarinense_regressão.cy.js
@@ -2,6 +2,6 @@
 
 import "../../Regressão/Catarinense/01_links_header.cy";
 import "./02_promo_cards.cy.js";
-import "../../Regressão/Catarinense/link_conexao.cy.js";
-import "../../Regressão/Catarinense/duvidas_frequentes.cy.js";
-import "../../Regressão/Catarinense/validar_links.cy.js";
+import "./03_link_conexao.cy.js";
+import "./04_duvidas_frequentes.cy.js";
+import "./05_validar_links.cy.js";
