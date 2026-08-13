3969f9f8131f6e4f09b12124aedb45416a4599cd
Anderson Silva dos Santos <anderson.ssantos@jcatlm.com.br>
2026-08-13 10:07:57 -0300

    TESTES APP SCRIPT

--- cypress/e2e/00_smoke/10_Totem.cy.js
+++ cypress/e2e/00_smoke/10_Totem.cy.js
@@ -28,7 +28,7 @@ describe("Totem", () => {
     cy.contains("button", "7").click();
     cy.wait(400);
     cy.get("#macAddress").click({ force: true }).type("241c04780308", { delay: 200 });
-    cy.get("#macAddress").click({ force: true }).type("241c04780308", { delay: 500 });
+    cy.get("#macAddress").click({ force: true }).clear().type("241c04780308", { delay: 500 });
     cy.get(".mt-4 > .rounded-lg").click({ force: true });
 
     // cy.get(".bg-primary").should("be.visible");
