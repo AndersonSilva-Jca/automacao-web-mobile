245e5825076116abeef715ad5ee94a751a30081c
Anderson Silva dos Santos <anderson.ssantos@jcatlm.com.br>
2026-08-25 16:01:28 -0300

    teste github actions

--- cypress/e2e/01_hml/10_Totem.cy.js
+++ cypress/e2e/01_hml/10_Totem.cy.js
@@ -26,45 +26,22 @@ describe("Totem", () => {
     cy.contains("button", "9").click();
     cy.wait(400);
     cy.contains("button", "7").click();
-    cy.get("#macAddress").click({ force: true }).type("241c04780308", { delay: 100 });
-    cy.get("#macAddress").click({ force: true }).type("241c04780308", { delay: 100 });
-    cy.get("#macAddress").click({ force: true }).type("241c04780308", { delay: 100 });
-    cy.get("#macAddress").click({ force: true }).type("241c04780308", { delay: 100 });
-    cy.get("#macAddress").click({ force: true }).type("241c04780308", { delay: 100 });
-    cy.get("#macAddress").click({ force: true }).type("241c04780308", { delay: 100 });
-    cy.get("#macAddress").click({ force: true }).type("241c04780308", { delay: 100 });
-    cy.get("#macAddress").click({ force: true }).type("241c04780308", { delay: 100 });
-    cy.get("#macAddress").click({ force: true }).type("241c04780308", { delay: 100 });
-    cy.get("#macAddress").click({ force: true }).type("241c04780308", { delay: 100 });
-    cy.get("#macAddress").click({ force: true }).type("241c04780308", { delay: 100 });
+    cy.get("#macAddress").click({ force: true }).clear().type("241c04780308");
+    cy.get("#macAddress").click({ force: true }).clear().type("241c04780308");
+    cy.get("#macAddress").click({ force: true }).clear().type("241c04780308");
+    cy.get("#macAddress").click({ force: true }).clear().type("241c04780308");
+    cy.get("#macAddress").click({ force: true }).clear().type("241c04780308");
+    cy.get("#macAddress").click({ force: true }).clear().type("241c04780308");
+    cy.get("#macAddress").click({ force: true }).clear().type("241c04780308");
+    cy.get("#macAddress").click({ force: true }).clear().type("241c04780308");
+    cy.get("#macAddress").click({ force: true }).clear().type("241c04780308");
+    cy.get("#macAddress").click({ force: true }).clear().type("241c04780308");
+    cy.get("#macAddress").click({ force: true }).clear().type("241c04780308");
+    cy.get("#macAddress").click({ force: true }).clear().type("241c04780308");
+
+    cy.get("#macAddress").click({ force: true }).clear().type("241c04780308", { delay: 100 });
+
     cy.get(".mt-4 > .rounded-lg").should("be.visible").click({ force: true });
-    cy.get("body").then(($body) => {
-      const erroAgencia = $body.find(".text-red-700");
-
-      if (erroAgencia.length > 0) {
-        const texto = erroAgencia.text().trim();
-
-        if (texto.includes("Agência não encontrada")) {
-          cy.log("⚠️ Agência não encontrada - comportamento conhecido no CI");
-
-          cy.get(".text-red-700").should("be.visible").and("contain.text", "Agência não encontrada");
-
-          // NÃO executa o fluxo abaixo
-          return;
-        }
-
-        throw new Error(`Erro inesperado na tela: ${texto}`);
-      } else {
-        // ===================================
-        // FLUXO NORMAL
-        // Só executa se NÃO existir o erro
-        // ===================================
-
-        cy.log("✅ Agência encontrada. Seguindo fluxo.");
-
-        cy.contains("button", "Comprar passagem", { timeout: 15000 }).should("be.visible").click();
-      }
-    });
     cy.get(".bg-primary > .flex-col > .justify-between > .flex").should("be.visible");
     cy.get(".bg-primary > .flex-col > .justify-between > .flex").click();
     // cy.get(":nth-child(3) > .bg-primary").click();
