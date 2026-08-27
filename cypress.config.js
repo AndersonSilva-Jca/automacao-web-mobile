bc059fca7c6131068386e306c68c4c55d9fb6500
Anderson Silva dos Santos <anderson.ssantos@jcatlm.com.br>
2026-08-27 19:35:33 -0300

    teste r2 cloudflare

--- cypress.config.js
+++ cypress.config.js
@@ -182,7 +182,7 @@ module.exports = defineConfig({
     charts: true,
     reportPageTitle: "Relatório de Testes em Produção - ODH/ODP/Giro/Wemobi/UTP",
     overwrite: false,
-    html: false,
+    html: true,
     json: true,
     saveJson: true,
     embeddedScreenshots: true,
