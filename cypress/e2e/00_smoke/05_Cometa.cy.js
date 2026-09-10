5605a44ef51fbaf1204c2449028ce90db617a389
Anderson Silva dos Santos <anderson.ssantos@jcatlm.com.br>
2026-09-10 15:48:48 -0300

    alteração os workflows

--- cypress/e2e/00_smoke/05_Cometa.cy.js
+++ cypress/e2e/00_smoke/05_Cometa.cy.js
@@ -47,4 +47,43 @@ describe("Viação Cometa", () => {
     // cy.get('.conditions-check', { timeout: 20000 }).click({ force: true })
     // cy.get('#payment-submit').should('be.visible').and('not.be.disabled').click();
   });
+
+  // it("Viação Cometa - Deve fazer login, busca de destinos, selecionar datas, seleção de passagens, selecionar assentos", () => {
+  //   cy.env(["login", "senha"]).then(() => {
+  //     cy.visit(cometa);
+  //     // LoginPage.abrirModalLogin();
+  //     // LoginPage.preencherUsuario();
+  //     // LoginPage.PreencherSenha();
+  //     // LoginPage.confirmarLogin();
+  //     // LoginPage.logadoComSucesso();
+  //   });
+
+  //   const origem = ["Campinas (SP)", "Santos (SP)", "Sorocaba (SP)", "São Paulo (Rod. Barra Funda) (SP)", "São Paulo (Rod. Tietê) (SP)"];
+  //   const indiceAleatorio = Math.floor(Math.random() * origem.length);
+  //   const origemSorteada = origem[indiceAleatorio];
+
+  //   cy.get(loc.BUSCAS.DESTINO_IDA).click().type(origemSorteada, { delay: 100 }).should("exist").invoke("show");
+  //   cy.get(`[class="ui-menu-item-wrapper "][id="${origemSorteada}"]`).click({ force: true });
+  //   cy.log(`Destino Sorteado e selecionado para o teste: ${origemSorteada}`);
+
+  //   cy.get(loc.BUSCAS.DESTINO_VOLTA).click().type(loc.RJ_TODOS, { delay: 100 }).should("exist").invoke("show");
+  //   cy.xpath(loc.XPATH_RJ_TODOS).click({ force: true });
+
+  //   // SearchPage.buscaOrigem();
+  //   // SearchPage.buscaDestino();
+  //   SearchPage.dataIda();
+  //   SearchPage.confirmarBusca();
+  //   OfferPage.selecionarPassagemIda();
+  //   PassengerPage.selecionarPassageiro();
+  //   SeatMapPage.selecionarAssento();
+  //   CheckoutPage.resumoDaCompra();
+
+  //   // cy.get('[alt="loader"]').should('not.be.visible')
+  //   // cy.url({ timeout: 90000 }).should('include', '/pagamento')
+  //   // Não finalizar a compra para evitar transações reais
+  //   // cy.get('[alt="loader"]').should('not.exist')
+  //   // cy.get('#tab-pix').click()
+  //   // cy.get('.conditions-check', { timeout: 20000 }).click({ force: true })
+  //   // cy.get('#payment-submit').should('be.visible').and('not.be.disabled').click();
+  // });
 });
