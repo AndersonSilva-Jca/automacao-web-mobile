2d54b2ee9770aa1215b7401c5e8993b43ffbe6e6
Anderson Silva dos Santos <anderson.ssantos@jcatlm.com.br>
2026-09-11 17:52:34 -0300

    alteração os workflows

--- cypress/pages/LoginPage.js
+++ cypress/pages/LoginPage.js
@@ -34,6 +34,7 @@ class LoginPage {
   odpPreencherUsuario() {
     cy.env(["login"]).then((env) => {
       cy.get(loc.USUARIO).type(env.login);
+      cy.get(loc.USUARIO).type("an@kdmsa.com.br");
     });
   }
 
